0a23fb7364bf33295d68b475dc43285c9a7aeade
naufan17 <muhammadnaufan719@yahoo.com>
2022-03-17 11:56:21 +0700

    background color gradient, responsive navigation menu, home section and new menu

---
+++ app.js
@@ -0,0 +1,21 @@
+const toggle = document.getElementById('ChangeTheme');
+const body = document.body;
+
+toggle.addEventListener('input', e => {
+    const isChecked = e.target.checked;
+
+    if (isChecked) {
+        body.classList.add("dark-mode");
+    } else {
+        body.classList.remove("dark-mode");
+    }
+});
+
+function myFunction() {
+    var x = document.getElementById("nav");
+    if (x.className === "header-menu") {
+      x.className += " responsive";
+    } else {
+      x.className = "header-menu";
+    }
+  }
