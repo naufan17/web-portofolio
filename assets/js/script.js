afb6efbf8f067409eefaeec4782bdc5d94bc4371
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2024-12-26 20:15:24 +0700

    refactor: custom croll, move script to different file and change show modal click

---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+function showSIAModal() {
+  document.getElementById('siaModal').classList.remove('hidden');
+}
+
+function closeSIAModal() {
+  document.getElementById('siaModal').classList.add('hidden');
+}
+
+function showConnectBCAModal() {
+  document.getElementById('connectBCAModal').classList.remove('hidden');
+}
+
+function closeConnectBCAModal() {
+  document.getElementById('connectBCAModal').classList.add('hidden');
+}
+
+function showBincangRakyatModal() {
+  document.getElementById('bincangRakyatModal').classList.remove('hidden');
+}
+
+function closeBincangRakyatModal() {
+  document.getElementById('bincangRakyatModal').classList.add('hidden');
+}
+
+function showMarvelModal() {
+  document.getElementById('marvelModal').classList.remove('hidden');
+}
+
+function closeMarvelModal() {
+  document.getElementById('marvelModal').classList.add('hidden');
+}
+
+function showCryptoModal() {
+  document.getElementById('cryptoModal').classList.remove('hidden');
+}
+
+function closeCryptoModal() {
+  document.getElementById('cryptoModal').classList.add('hidden');
+}
+
+function showCekResiModal() {
+  document.getElementById('cekResiModal').classList.remove('hidden');
+}
+
+function closeCekResiModal() {
+  document.getElementById('cekResiModal').classList.add('hidden');
+}
+
+function showBCRModal() {
+  document.getElementById('bcrModal').classList.remove('hidden');
+}
+
+function closeBCRModal() {
+  document.getElementById('bcrModal').classList.add('hidden');
+}
+
+function showPortfolioModal() {
+  document.getElementById('portfolioModal').classList.remove('hidden');
+}
+
+function closePortofolioModal() {
+  document.getElementById('portfolioModal').classList.add('hidden');
+}
+
+function showSimplifyModal() {
+  document.getElementById('simplifyModal').classList.remove('hidden');
+}
+
+function closeSimplifyModal() {
+  document.getElementById('simplifyModal').classList.add('hidden');
+}
